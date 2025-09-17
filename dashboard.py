e3fb12f58cd0cde0ed92fc6d7cb7d14b7261fc0d
eliblouin <elipblouin@gmail.com>
2025-09-17 17:18:35 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -41,7 +41,7 @@ def website_setup(df, colork = 'yellow'):
         st.link_button("View Challenge Sheet", challenge_url)
     # st.link_button("Register Your Team", register_url)
     # st.link_button("Submit a Challenge", submit_url)
-    st.text("Updated on 9/17/2025 at 3:40PM EST")
+    st.text("Updated on 9/17/2025 at 5:15PM EST")
     img = Image.open("mentorship.png")
     st.image(img, width=700)
     # Load CSV
@@ -105,6 +105,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
